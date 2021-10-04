ae445c4c9071006e4a30cbc2e16e744cd26de366
uberballo <kasper.paavolainen@gmail.com>
2021-10-04 22:12:14 +0300

    Add day6b

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -4,24 +4,41 @@ use std::iter::FromIterator;
 
 pub fn solve(input: String) {
     let data = helpers::file::to_combined_list(&input);
-    part1(data);
-    part2();
+    part1(&data);
+    part2(&input);
 }
 
 fn combine_to_one(input: &str) -> String {
     input.replace("\n", "")
 }
 
-fn part1(input: Vec<&str>) {
-    let res = input
+fn group_data(input: &String) -> Vec<Vec<HashSet<char>>> {
+    input
+        .split("\n\n")
+        .map(|x| x.lines().map(|i| HashSet::from_iter(i.chars())).collect())
+        .collect::<Vec<Vec<HashSet<char>>>>()
+}
+
+fn count_votes_unique(input: &Vec<&str>) -> Vec<HashSet<char>> {
+    input
         .iter()
         .map(|x| HashSet::from_iter(combine_to_one(x).chars()))
-        .collect::<Vec<HashSet<char>>>();
+        .collect::<Vec<HashSet<char>>>()
+}
 
+fn part1(input: &Vec<&str>) {
+    let res = count_votes_unique(input);
     let sum: usize = res.iter().map(|x| x.len()).sum();
     println!("{}", sum);
 }
+fn intersect_sets(sets: &[HashSet<char>]) -> HashSet<char> {
+    sets.iter().fold(sets[0].clone(), |s1, s2| {
+        s1.intersection(&s2).copied().collect()
+    })
+}
 
-fn part2() {
-    println!("Not implemented");
+fn part2(input: &String) {
+    let res = group_data(input);
+    let sum: usize = res.iter().map(|x| intersect_sets(x).len()).sum();
+    println!("{}", sum)
 }
